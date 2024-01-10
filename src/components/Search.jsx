f91b2f2dce9ad63e40310a72713a64ae8efd4507
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-01-10 12:37:38 -0700

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import {useState} from 'react'
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(
+      searchTerm
+    )}`;
+    window.open(searchUrl, '_blank');
+  };
+
   return (
-    <div style={{ border: 'thin', padding: '2rem', margin: '1rem' }}>
-      Google Search
-      <br/>
-    <form action="http://google.com" method="get" target="_blank">
-        <input type="text" name="bar" maxLength={"255"} id="bar" />
-        <input type="submit" value="Search"></input>
-    </form>
+    <div style={{ border: '1px solid #ccc', padding: '1rem', margin: '1rem' }}>
+      <label htmlFor='searchInput'>Google Search</label>
+      <br />
+      <form onSubmit={handleSearch}>
+        <input
+          type='text'
+          id='searchInput'
+          name='searchInput'
+          maxLength={255}
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        <button type='submit'>Search</button>
+      </form>
     </div>
   );
-}
+};
 
 export default Search
